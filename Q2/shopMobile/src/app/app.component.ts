ff2558b56d312a69f5d58b0215d7f8b08a9439e0
powerofwind <powerofwind10@gmail.com>
2019-01-16 10:25:16 +0700

    add cartpage

--- Q2/shopMobile/src/app/app.component.ts
+++ Q2/shopMobile/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CartPage } from '../pages/cart/cart';
 
 @Component({
   templateUrl: 'app.html'
@@ -21,8 +22,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'AddProduct', component: HomePage },
+      { title: 'Shopping', component: ListPage },
+      { title: 'Cart', component: CartPage },
     ];
 
   }
