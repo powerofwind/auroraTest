d7200021ae46112ae839a7bb97e75f7a39242ab3
powerofwind <powerofwind10@gmail.com>
2019-01-16 10:31:19 +0700

    add HttpClient

--- Q2/shopMobile/src/app/app.module.ts
+++ Q2/shopMobile/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CartPage } from '../pages/cart/cart';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { CartPage } from '../pages/cart/cart';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
