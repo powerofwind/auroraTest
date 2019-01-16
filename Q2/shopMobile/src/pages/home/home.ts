ebf7466fc4b1f4b25917cefa1a70476fe7e9582b
powerofwind <powerofwind10@gmail.com>
2019-01-16 10:48:29 +0700

    Implement code in html

--- Q2/shopMobile/src/pages/home/home.ts
+++ Q2/shopMobile/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
@@ -10,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController,public http: HttpClient) {
 
+  }
+
+  add(){
+    this.navCtrl.push(ListPage)
   }
 
 }
