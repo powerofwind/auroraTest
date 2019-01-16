72abcf02197bad129467f61bc4f1e0a9be481462
powerofwind <powerofwind10@gmail.com>
2019-01-16 15:44:50 +0700

    add post*api

---
+++ Q1/interestAPI/Controllers/InterestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace interestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestController : ControllerBase
+    {
+        public static List<interest> allinterest = new List<interest>();
+
+        [HttpPost]
+        public void Post([FromBody] interest value)
+        {
+            var newinterest = new interest
+            {
+                Interest = value.Interest,
+                
+            };
+            allinterest.Add(newinterest);
+
+        }
+
+      
+        [HttpGet]
+        public ActionResult<IEnumerable<interest>> Get()
+        {
+            return allinterest;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+
+
+
+    }
+}
