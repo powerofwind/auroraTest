af688cc72c9fdf21bf513667032d962909f0e89c
powerofwind <powerofwind10@gmail.com>
2019-01-16 15:32:44 +0700

    implementcode ui and create model

---
+++ Q1/interestAPI/Models/interest.ts
@@ -0,0 +1,11 @@
+namespace interestAPI.Controllers
+{
+    public class interest
+    {
+        public string Yearcount { get; set; }
+        public int Money { get; set; }
+        public int Interest { get; set; }
+        public int Pay { get; set; }
+    }
+}
+
---
+++ Q1/interestMobile/src/models/interest.ts
@@ -0,0 +1,6 @@
+export class interest {
+    public yearcount: string;
+    public money: number;
+    public interest: number;
+    public pay: number;
+}
