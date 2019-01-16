58818584f1d24ba66ca6e973ce71b73b7a2bedd4
powerofwind <powerofwind10@gmail.com>
2019-01-16 15:14:00 +0700

    2testcase pass

--- Q1/interestAPI/logic.cs
+++ Q1/interestAPI/logic.cs
@@ -4,19 +4,19 @@ namespace interestAPI
     {
         public double interestLogic(double inputMoney, double inputInterest, int inputYear)
         {
+
             double total = 0;
             double pay = 0;
 
             double interest = (inputInterest / 100);
             double useInterest = inputMoney * (interest);
 
-            for (int i = 0; i < inputYear; i++)
-            {
+            
                 pay = inputMoney + useInterest;
                 total += pay;
-            }
+            
 
-            return 0;
+            return total;
         }
     }
 }
--- Q1/interestTest/UnitTest1.cs
+++ Q1/interestTest/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using interestAPI;
 using Xunit;
 
 namespace interestTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory(DisplayName = "เงินต้น10k ,ดอก12%, 3ปี")]
+        [InlineData(10000,12,3,11200)]
+        public void money10k(double inputMoney, double inputInterest, int inputYear,double expected)
         {
+            var sut = new logic();
+            var result = sut.interestLogic(inputMoney,inputInterest,inputYear);
+            Assert.Equal(expected, result);
+        }
 
+        [Theory(DisplayName = "เงินต้น20k ,ดอก5%, 3ปี")]
+        [InlineData(20000,5,10,21000)]
+        public void money20k(double inputMoney, double inputInterest, int inputYear,double expected)
+        {
+            var sut = new logic();
+            var result = sut.interestLogic(inputMoney,inputInterest,inputYear);
+            Assert.Equal(expected, result);
         }
     }
 }
